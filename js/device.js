a26c206dc4c3cda9c7655fc729d3ee71264dcd5a
quyendt4 <quyendt4@topica.edu.vn>
2018-11-16 16:49:31 +0700

    device ok

--- js/device.js
+++ js/device.js
@@ -1,16 +1,18 @@
-const domain = "http://171.244.4.48:6969/api/device/";
+// const domain = "http://171.244.4.48:6969/api/";
+const domain = "http://localhost:6969/api/";
 var responseList = null;
 var page = 0;
+
 $( document ).ready(function() {
 
   console.log( "ready!" );
   getDevices();
-
+  showListDepartment();
 });
 
 
 function getDevices(){
-  const url = domain+'list?page='+page+'&size=10'
+  const url = domain+'device/list?page='+page+'&size=15'
   var request = new Request(url, {
     method: 'GET',
     headers: new Headers()
@@ -25,7 +27,9 @@ function getDevices(){
     responseList = json;
     console.log(json.content);
     loadDevice(json.content);
-    emptyFooterTable();
+    if(page == responseList.totalPages - 1){
+      disableNextButton();
+    }
   })
 }
 
@@ -38,15 +42,6 @@ function loadDevice(listDevice){
                                         .replace("%DATEIN%", convertTimestampToDate(device.dateIn))
                                         .replace(/%ID%/g,device.id))
   });
-}
-
-function emptyFooterTable(){
-  $(".dataTables_info").parent().parent().empty();
-  console.log("emptyFooterTable"+responseList.pageable.pageNumber)
-  if(!responseList.totalPages){
-    disableNextButton();
-  }
-
 }
 
 function disableNextButton(){
@@ -80,23 +75,66 @@ function convertTimestampToDate(timestamp){
   return date.toLocaleDateString();
 }
 
+function convertDateToTimestamp(date){
+  if(!date){
+    return null;
+  }
+
+  return Date.parse(date);
+}
+
+function showListDepartment(){
+  const url = domain+'department/list?page=0&size=200';
+  console.log(url);
+  var request = new Request(url, {
+    method: 'GET',
+    headers: new Headers()
+  })
+
+  fetch(request).then(function (response) {
+    if (response.ok) {
+      return response.json()
+    }
+    throw new Error('(getListDepartment)ERROR')
+  }).then(function (json) {
+    if(json.content){
+      console.log("getListDepartment");
+      console.log(json.content);
+      var listDepartment = _.map(json.content, 'name');
+      console.log(listDepartment);
+      loadDepartment(listDepartment);
+    }
+  })
+}
+
+function loadDepartment(listDepartment){
+  $('.device-department').empty();
+  console.log('loadDepartment');
+  console.log(listDepartment);
+  if(listDepartment){
+    listDepartment.forEach(department=>{
+      $('.device-department').append(DEPARTMENT_OPTION_ITEM.replace(/%NAME%/g,department));
+    })
+  }
+}
+
 const DEVICE_ITEM = "<tr>"
     + "                  <td>%NAME%</td>"
     + "                  <td>%CODE%</td>"
     + "                  <td>%PRICE%</td>"
     + "                  <td>%DATEIN%</td>"
     + "                  <td>"
-    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-history\">"
+    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-history\" onclick='onOpenUseHistory(%ID%)'>"
     + "                      Xem"
     + "                    </button>"
     + "                  </td>"
     + "                  <td>"
-    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-repair\">"
+    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-repair\" onclick='onOpenRepairHistory(%ID%)'>"
     + "                      Xem"
     + "                    </button>"
     + "                  </td>"
     + "                  <td>"
-    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-detail\">"
+    + "                    <button type=\"button\" class=\"btn btn-default\" value='%ID%' data-toggle=\"modal\" data-target=\"#modal-detail\" onclick='onOpenDeviceDetail(this)' >"
     + "                      Xem"
     + "                    </button>"
     + "                  </td>"
@@ -107,4 +145,54 @@ const DEVICE_ITEM = "<tr>"
     + "                  </td>"
     + "              </tr>"
 
+const USE_HISTORY_ITEM ="<tr>"
+    + "                  <td>%DAY%</td>"
+    + "                  <td>%CONTENT%</td>"
+    + "                  <td>%FOLLOWER%</td>"
+    + "                 </tr>"
 
+const REPAIR_HISTORY_ITEM = "<tr>"
+    + "                  <td>%DAY%</td>"
+    + "                  <td>%CONTENT%</td>"
+    + "                  <td>%LEVEL%</td>"
+    + "                  <td>%FOLLOWER%</td>"
+    + "                      </tr>";
+
+const DEPARTMENT_OPTION_ITEM = '<option value="%NAME%">%NAME%</option>';
+
+const ADD_USE_HISTORY = "<tr>"
+    + "                  <td>"
+    + "                    <div class=\"input-group date\">"
+    + "                      <div class=\"input-group-addon\">"
+    + "                        <i class=\"fa fa-calendar\"></i>"
+    + "                      </div>"
+    + "                      <input type=\"text\" class=\"form-control pull-right use-history-date\" id=\"datePickerDateUseHistory\" required>"
+    + "                    </div>"
+    + "                  </td>"
+    + "                  <td>"
+    + "                    <input type=\"text\" class=\"form-control use-historycontent\" required>"
+    + "                  </td>"
+    + "                  <td>"
+    + "                    <input type=\"text\" class=\"form-control use-history-follower\" required>"
+    + "                  </td>"
+    + "                </tr>"
+
+const ADD_REPAIR_HISTORY = "<tr>"
+    + "                  <td>"
+    + "                    <div class=\"input-group date\">"
+    + "                      <div class=\"input-group-addon\">"
+    + "                        <i class=\"fa fa-calendar\"></i>"
+    + "                      </div>"
+    + "                      <input type=\"text\" class=\"form-control pull-right repair-history-date\" id=\"datePickerDateRepair\" required>"
+    + "                    </div>"
+    + "                  </td>"
+    + "                  <td>"
+    + "                    <input type=\"text\" class=\"form-control repair-content\" required>"
+    + "                  </td>"
+    + "                  <td>"
+    + "                    <input type=\"number\" class=\"form-control repair-level\" required>"
+    + "                  </td>"
+    + "                  <td>"
+    + "                    <input type=\"text\" class=\"form-control repair-follower\" required>"
+    + "                  </td>"
+    + "                </tr>"
--- js/deviceAction.js
+++ js/deviceAction.js
@@ -1,4 +1,7 @@
 function onNextPage(){
+  if(page == responseList.totalPages - 1){
+    return;
+  }
   console.log("onNextPage"+page);
   page++;
   console.log("onNextPage2"+page);
@@ -10,6 +13,9 @@ function onNextPage(){
 }
 
 function onPrePage(){
+  if(!page){
+    return;
+  }
   console.log("onPrePage");
   page = page - 1;
   getDevices();
@@ -21,8 +27,40 @@ function onPrePage(){
 
 function onDelete(element){
   console.log("onDelete");
-  var deviceId = element.value;
-  const url = domain+'delete?deviceId='+deviceId;
+
+  var r = confirm("Có chắc chắn muốn xóa?");
+  if (r == true) {
+    var deviceId = element.value;
+    const url = domain+'device/delete?deviceId='+deviceId;
+    var request = new Request(url, {
+      method: 'GET',
+      headers: new Headers()
+    })
+
+    fetch(request).then(function (response) {
+      if (response.ok) {
+        return response.json()
+      }
+      throw new Error('(onDelete)ERROR')
+    }).then(function (json) {
+      console.log("delete done");
+      console.log(json);
+    })
+    alert("Xóa thành công!")
+    getDevices();
+
+  }
+
+}
+
+function onOpenUseHistory(element){
+  console.log('onOpenUseHistory');
+  $('.add-use-history-button').show();
+  $('.save-use-history-button').hide();
+  var deviceId = element;
+  console.log("onOpenUseHistory: ", deviceId);
+  $('#use-history-table').val(deviceId);
+  const url = domain+'useHistory/getByDevice?page=0&size=100&deviceId='+deviceId;
   var request = new Request(url, {
     method: 'GET',
     headers: new Headers()
@@ -32,11 +70,317 @@ function onDelete(element){
     if (response.ok) {
       return response.json()
     }
-    throw new Error('(onDelete)ERROR')
+    throw new Error('(onOpenUseHistory)ERROR')
   }).then(function (json) {
-    console.log("delete done");
-    console.log(json);
+    if(json.content){
+      showUseHistory(json.content);
+    }
+
+  })
+}
+
+function showUseHistory(data){
+  $(".use-history-content").empty();
+  data.forEach(item =>{
+    $(".use-history-content").append(USE_HISTORY_ITEM.replace('%DAY%',convertTimestampToDate(item.dateTime))
+                                                   .replace('%CONTENT%', item.content)
+                                                   .replace('%FOLLOWER%', item.follower));
+  });
+}
+
+function onAddUseHistory(){
+  console.log('onAddUseHistory');
+  $('.add-use-history-button').hide();
+  $('.save-use-history-button').show();
+  $(".use-history-content").append(ADD_USE_HISTORY);
+  $('#datePickerDateUseHistory').datepicker({
+    autoclose: true
   })
 
-  getDevices();
 }
+
+$('.use-history-form').submit(()=>{
+  console.log('onSubmitUseHistory');
+  var deviceId = $('#use-history-table').val();
+  var dateTime = convertDateToTimestamp($('.use-history-date').val());
+  var content = $('.use-historycontent').val()
+  console.log("dateTime "+ dateTime)
+  var useHistory = JSON.stringify({
+    'deviceId' : deviceId,
+    'dateTime' : dateTime,
+    'content' : content,
+    'follower' : $('.use-history-follower').val()
+  });
+
+  var settings = {
+    "crossDomain": true,
+    "url": domain+"/useHistory/save",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "data": useHistory
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log("ajax");
+    console.log(response);
+    if(response.toUpperCase() == 'SUCCESS'){
+      alert('Lưu thành công!');
+      onOpenUseHistory(deviceId);
+
+    }else{
+      alert('Có lỗi trong quá trình thêm. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+    }
+  }).fail((response)=>{
+    alert('Có lỗi trong quá trình thêm. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+  });
+
+
+});
+
+
+function onOpenRepairHistory(element){
+  $('.add-repair-history-button').show();
+  var deviceId = element;
+  console.log("onOpenRepairHistory: ", deviceId);
+  $('.save-repair-history').hide();
+  $('#repair-history-table').val(deviceId);
+  const url = domain+'repairHistory/getByDevice?page=0&size=20&deviceId='+deviceId;
+  var request = new Request(url, {
+    method: 'GET',
+    headers: new Headers()
+  })
+
+  fetch(request).then(function (response) {
+    if (response.ok) {
+      return response.json()
+    }
+    throw new Error('(onOpenRepairHistory)ERROR')
+  }).then(function (json) {
+    console.log("onOpenRepairHistory data");
+    console.log(json.content)
+    if(json.content){
+      showRepairHistory(json.content);
+    }
+  })
+}
+
+function showRepairHistory(data){
+  $(".repair-history-content").empty();
+  data.forEach(item =>{
+    $(".repair-history-content").append(REPAIR_HISTORY_ITEM.replace('%DAY%',convertTimestampToDate(item.dateTime))
+                                                   .replace('%CONTENT%', item.content)
+                                                   .replace('%LEVEL%', item.level)
+                                                   .replace('%FOLLOWER%', item.follower));
+  });
+}
+
+function onAddRepairHistory(){
+  console.log('onAddRepairHistory');
+  $('.save-repair-history').show();
+  $('.repair-history-content').append(ADD_REPAIR_HISTORY);
+  $('#datePickerDateRepair').datepicker({
+    autoclose: true
+  })
+  $('.add-repair-history-button').hide();
+}
+
+$('.add-repair-form').submit(()=>{
+  console.log('submit repair history');
+
+  var deviceId = $('#repair-history-table').val();
+  var dateTime = convertDateToTimestamp($('.repair-history-date').val());
+  var content = $('.repair-content').val();
+  var level = $('.repair-level').val();
+
+  console.log("dateTime "+ dateTime)
+  var useHistory = JSON.stringify({
+    'deviceId' : deviceId,
+    'dateTime' : dateTime,
+    'content' : content,
+    'level': level,
+    'follower' : $('.repair-follower').val()
+  });
+
+  var settings = {
+    "crossDomain": true,
+    "url": domain+"/repairHistory/save",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "data": useHistory
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log("ajax");
+    console.log(response);
+    if(response.toUpperCase() == 'SUCCESS'){
+      alert('Lưu thành công!');
+      onOpenRepairHistory(deviceId);
+
+    }else{
+      alert('Có lỗi trong quá trình lưu. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+    }
+  }).fail((response)=>{
+    alert('Có lỗi trong quá trình lưu. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+  });
+
+});
+
+function onOpenDeviceDetail(element){
+  $('.device-info').prop("disabled", true);
+  $('.device-info').val('');
+  console.log("onOpenDeviceDetail");
+  var deviceId = element.value;
+
+  const url = domain+'device/getById?id='+deviceId;
+  var request = new Request(url, {
+    method: 'GET',
+    headers: new Headers()
+  })
+
+  fetch(request).then(function (response) {
+    if (response.ok) {
+      return response.json()
+    }
+    throw new Error('(onOpenDeviceDetail)ERROR')
+  }).then(function (json) {
+    if(json){
+      console.log(json);
+      showDeviceInfo(json);
+    }
+
+  })
+
+
+}
+
+function showDeviceInfo(device){
+  $('.device-id').val(device.id);
+  $('.device-code').val(device.code);
+  $('.device-id').val(device.id);
+  $('.device-name').val(device.name);
+  $('.device-number').val(device.number);
+  $('.device-madeIn').val(device.madeIn);
+  $('.device-madeYear').val(device.madeYear);
+  $('.device-department').val(device.department);
+  $('.device-dateReceived').val(convertTimestampToDate(device.dateReceived));
+  $('.device-dateUse').val(convertTimestampToDate(device.dateUse));
+  $('.device-dateIn').val(convertTimestampToDate(device.dateIn));
+  $('.device-price').val(toSeparatedNumber(device.price));
+  $('.device-description').val(toSeparatedNumber(device.description));
+  $('.device-voltageFrom').val(device.voltageFrom);
+  $('.device-voltageTo').val(device.voltageTo);
+  $('.device-ampe').val(device.ampe);
+  $('.device-capacity').val(device.capacity);
+  $('.device-accessory').val(device.accessory);
+  $('.device-receiver').val(device.receiver);
+  $('.device-moneySource').val(device.moneySource);
+  $('.device-statusReceipt').val(device.statusReceipt);
+  $('.device-levelQuality').val(device.levelQuality);
+}
+
+function onOpenAdd(){
+  $('.device-info').prop("disabled", false);
+  $('.device-info').val("");
+  $('.save-device-button').show();
+
+}
+
+function onSearch(){
+  console.log('onSearch');
+  var searchValue = $(".search-device").val();
+  if(!searchValue){
+    getDevices();
+  }
+  var searchType = $(".search-type option:selected").val();
+
+  const url = domain+'/device/search?page=0&size=15&'+searchType+'='+searchValue;
+  console.log(url);
+  var request = new Request(url, {
+    method: 'GET',
+    headers: new Headers()
+  })
+
+  fetch(request).then(function (response) {
+    if (response.ok) {
+      return response.json()
+    }
+    throw new Error('(onOpenUseHistory)ERROR')
+  }).then(function (json) {
+    if(json.content){
+      console.log(json.content);
+      loadDevice(json.content);
+    }
+
+  })
+}
+
+
+function onSubmitAddDevice() {
+  console.log("onSubmitAddDevice");
+}
+
+$('.device-form').submit(()=>{
+  console.log("onSubmit");
+
+  var device = JSON.stringify({
+    'id' : $('.device-id').val(),
+    'code' : $('.device-code').val(),
+    'name' : $('.device-name').val(),
+    'number' : $('.device-number').val(),
+    'madeIn' : $('.device-madeIn').val(),
+    'madeYear':$('.device-madeYear').val(),
+    'department' : $('.device-department').val(),
+    'dateReceived' : convertDateToTimestamp($('.device-dateReceived').val()),
+    'dateUse' : convertDateToTimestamp($('.device-dateUse').val()),
+    'dateIn' : convertDateToTimestamp($('.device-dateIn').val()),
+    'price' : $('.device-price').val(),
+    'description':$('.device-description').val(),
+    'voltageFrom':$('.device-voltageFrom').val(),
+    'voltageTo':$('.device-voltageTo').val(),
+    'ampe':$('.device-ampe').val(),
+    'capacity':$('.device-capacity').val(),
+    'accessory':$('.device-accessory').val(),
+    'receiver':$('.device-receiver').val(),
+    'moneySource':$('.device-moneySource').val(),
+    'statusReceipt':$('.device-statusReceipt').val(),
+    'levelQuality':$('.device-levelQuality').val()
+  });
+  console.log(device);
+
+
+  var settings = {
+    "crossDomain": true,
+    "url": domain+"/device/save",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "data": device
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log("ajax");
+    console.log(response);
+    if(response.toUpperCase() == 'SUCCESS'){
+      alert('Lưu thành công!');
+      $('.btn-close-detail').click();
+      getDevices();
+    }else{
+      alert('Có lỗi trong quá trình thêm. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+    }
+  }).fail((response)=>{
+    alert('Có lỗi trong quá trình thêm. Vui lòng liên hệ kỹ thuật để được hỗ trợ!');
+  });
+
+})
+
+function onEdit(input){
+  console.log("onEdit: "+input);
+  $('.'+input).prop("disabled", false);
+  $('.save-device-button').show();
+}
+
